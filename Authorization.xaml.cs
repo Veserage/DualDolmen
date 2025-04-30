61182a70b08a7df2334412c6fac60466baada8f1
Slazzzer <greegedward93@gmail.com>
2025-04-30 22:40:43 +0300

    1) Добавил некоторые картинки для приложения (все в сборке типа ресурс) 2) Сделал главную страницу авторизации 3) Начал описывать класс UsersApp для паролей пользователей.

---
+++ Authorization.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DualDolmen
+{
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+            LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            var uri = new Uri("/Images/London.jpg", UriKind.Relative);
+            MainImage.Source = new BitmapImage(uri);
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Завтра продолжим!");
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("А пока спать!");
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,30 +1,13 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace DualDolmen
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            
             InitializeComponent();
-        }
-
-        private void Gleb_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Глеб, када на МАЛАДЁШКУ?", "Увага", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainFrame.Navigate(new Authorization());
         }
     }
 }
